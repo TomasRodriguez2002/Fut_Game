b6241503e05e4dab40710b1ec75646a72f63accd
TomasRodriguez2002 <tomasrodriguez02002@gmail.com>
2024-01-20 11:19:37 -0300

    Actualizacion de codigo: agregado de contador de tiempo, prueba de interfaz y correcion de errores como jugar con un solo equipo

--- Game.py
+++ Game.py
@@ -17,6 +17,9 @@ class Game(object):
     def __init__(self, team1_name, team2_name):
         self.team1_name = team1_name
         self.team2_name = team2_name
+        self.initialize_game()
+
+    def initialize_game(self):
         self.team2 = None
         self.team1 = None
         self.ball = None
@@ -24,54 +27,61 @@ class Game(object):
         self.goal_message = None
         self.current_goal_message = None
         self.sprites = pygame.sprite.Group()
-        self.initialize_game()
-
-    def initialize_game(self):
+        self.total_ticks = 0
+        self.minutes = 0
+        self.seconds = 0
+        self.team1_created = False
         self.goal_message = None         
         self.current_goal_message = None
         self.sprites = pygame.sprite.Group()
         self.mediator = Mediator()
         self.ball = Ball(self, self.mediator, "ball.png")
 
-        if self.team1_name == "braian":
+        if self.team1_name == "braian" and not self.team1_created:
             braianFactory = TeamBraianFactory("sprites/playerNico(Peruano).png", self.mediator, True, 5)
             self.team1 = braianFactory.createTeam()
-        elif self.team2_name == "braian":
+            self.team1_created = True
+        if self.team2_name == "braian":
             braianFactory = TeamBraianFactory("sprites/playerNico(Peruano).png", self.mediator, False, 5)
             self.team2 = braianFactory.createTeam()
             
-        if self.team1_name == "mateo":
+        if self.team1_name == "mateo" and not self.team1_created:
             mateoFactory = TeamMateoFactory("sprites/playerNico(Peruano).png", self.mediator, True, 5)
             self.team1 = mateoFactory.createTeam()
-        elif self.team2_name == "mateo":
+            self.team1_created = True
+        if self.team2_name == "mateo":
             mateoFactory = TeamMateoFactory("sprites/playerNico(Peruano).png", self.mediator, False, 5)
             self.team2 = mateoFactory.createTeam()
 
-        if self.team1_name == "gonzalo":
+        if self.team1_name == "gonzalo" and not self.team1_created:
             gonzaloFactory = TeamGonzaloFactory("sprites/playerNico(Peruano).png", self.mediator, True, 5)
             self.team1 = gonzaloFactory.createTeam()
-        elif self.team2_name == "gonzalo":
+            self.team1_created = True
+        if self.team2_name == "gonzalo":
             gonzaloFactory = TeamGonzaloFactory("sprites/playerNico(Peruano).png", self.mediator, False, 5)
             self.team2 = gonzaloFactory.createTeam()
 
-        if self.team1_name == "nicolas":
+        if self.team1_name == "nicolas" and not self.team1_created:
             nicolasFactory = TeamNicolasFactory("sprites/playerNico(Peruano).png", self.mediator, True, 5)
             self.team1 = nicolasFactory.createTeam()
-        elif self.team2_name == "nicolas":
+            self.team1_created = True
+        if self.team2_name == "nicolas":
             nicolasFactory = TeamNicolasFactory("sprites/playerNico(Peruano).png", self.mediator, False, 5)
             self.team2 = nicolasFactory.createTeam()
 
-        if self.team1_name == "tomas_r":
-            tomasRFactory = TeamTomasRFactory("sprites/playerNico(Peruano).png", self.mediator, True, 5)
+        if self.team1_name == "tomas_r" and not self.team1_created:
+            tomasRFactory = TeamTomasRFactory("player.png", self.mediator, True, 5)
             self.team1 = tomasRFactory.createTeam()
-        elif self.team2_name == "tomas_r":
-            tomasRFactory = TeamTomasRFactory("sprites/playerNico(Peruano).png", self.mediator, False, 5)
+            self.team1_created = True
+        if self.team2_name == "tomas_r":
+            tomasRFactory = TeamTomasRFactory("player.png", self.mediator, False, 5)
             self.team2 = tomasRFactory.createTeam()
 
-        if self.team1_name == "tomas_g":
+        if self.team1_name == "tomas_g" and not self.team1_created:
             tomasGFactory = TeamTomasGFactory("sprites/playerNico(Peruano).png", self.mediator, True, 5)
             self.team1 = tomasGFactory.createTeam()
-        elif self.team2_name == "tomas_g":
+            self.team1_created = True
+        if self.team2_name == "tomas_g":
             tomasGFactory = TeamTomasGFactory("sprites/playerNico(Peruano).png", self.mediator, False, 5)
             self.team2 = tomasGFactory.createTeam()
         
@@ -117,6 +127,13 @@ class Game(object):
 
     def run_logic(self):
         self.sprites.update()
+        self.total_ticks += 1
+        # Actualizar el tiempo cada segundo (30 ticks)
+        if self.total_ticks % 30 == 0:
+            self.seconds += 1
+            if self.seconds == 60:
+                self.minutes += 1
+                self.seconds = 0
 
     def display_frame(self, screen, background):
         screen.blit(background, [0 , 0])
@@ -127,6 +144,13 @@ class Game(object):
             self.goal_message_timer -= 1
             if self.goal_message_timer <= 0:
                 self.current_goal_message = None  # Borra el mensaje cuando el temporizador llega a cero
+            
+        # Mostrar el contador de tiempo en la parte superior al centro
+        timer_text = f"{self.minutes:02d}:{self.seconds:02d}"
+        timer_surface = font.render(timer_text, True, (255, 255, 255))
+        timer_position = ((WIDTH - timer_surface.get_width()) // 2, 20)
+        screen.blit(timer_surface, timer_position)
+
         pygame.display.flip()
 
     def show_goal_message(self, message, duration):
