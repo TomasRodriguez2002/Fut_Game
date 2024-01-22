fee5c90ac5178bf311b042fd84c2ab8386f6bc2e
Gonzalibrandi <librandigonzalo@gmail.com>
2024-01-21 22:59:32 -0500

    modulo de pase y de cuando se tiene la pelota

--- Strategies/GonzaloStrategy.py
+++ Strategies/GonzaloStrategy.py
@@ -1,14 +1,38 @@
 from Strategies.Strategy import Strategy
+import math
+from Constantes import *
 
 class GonzaloStrategy(Strategy):
     def __init__(self):
         super().__init__()
-       
+    
     def getProxPos(self, player):
-        pass
+        return 3
 
     def with_ball(self, player):
-        pass
-
-    def where_to_pass(self, player):
-        pass
+        # si estoy en el area rival pateo
+        if ((player.team==True) and (player.rect.centerx > AREA_G_MID_DER) and (player.rect.centery<AREA_C_SUP) and (player.rect.centery>AREA_C_INF)) or ((player.team==False) and (player.rect.centerx < AREA_G_MID_IZQ) and (player.rect.centery<AREA_C_SUP) and (player.rect.centery>AREA_C_INF)):
+            return 1
+        # si no estoy en el area, la paso
+        return 2
+        
+    # se la paso al compañero mas cercano
+    def where_to_pass(self,player):
+        teammates = player.mediator.getTeammates(player.team)
+        min_distance = float('inf')
+        nearest_teammate = None
+        for teammate in teammates:
+            if(teammate!=player):
+                actual_distance = self.distance_to_player(self,player,teammate)
+                if(actual_distance<min_distance):
+                    min_distance=actual_distance
+                    nearest_teammate = teammate
+        return nearest_teammate
+    
+    #distancia entre dos jugadores
+    def distance_to_player(self, player1, player2):
+        return math.sqrt((player2.rect.centerx - player1.rect.centerx)**2 + (player2.rect.centery - player1.rect.centery)**2)
+    
+    #distancia entre un jugador y la pelota
+    def distance_to_ball(self, player, ball_centerx, ball_centery):
+        return math.sqrt((ball_centerx - player.rect.centerx)**2 + (ball_centery - player.rect.centery)**2)
